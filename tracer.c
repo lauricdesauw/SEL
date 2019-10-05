5833555cba4d110b4db9803b9a32045c41487ac5
Lauric Desauw <lauric@Jace.localdomain>
2019-10-05 14:45:58 +0200

    intros variables fscanf

--- tracer.c
+++ tracer.c
@@ -4,29 +4,40 @@
 
 int main (int argc, char** argv)
 {
-     // Getting the pid of the tracee process
+	// Getting the pid of the tracee process
 
-     if( execl("/bin/pgrep", "tracee",NULL) < 0)
-     {}
-    
-     pid_t pid;
-     if(fread(&pid,sizeof(pid_t),1,stdout) < 0)
-     {}
+	if( execl("/bin/pgrep", "tracee",NULL) < 0)
+	{}
 
-     // Attaching to the tracee process
-     
-     if(ptrace(PTRACE_ATTACH, pid,NULL, NULL) < 0)
-     {}
-     
-     if(waitpid(pid, NULL, 0) < 0)
-     {}
-     
-     // Looking for foo in the tracee binary
-     
-     // Finding the call to foo
+	pid_t pid;
+	if(fread(&pid,sizeof(pid_t),1,stdout) < 0)
+	{}
 
-     // Writing our function
-     
-     // Replacing foo by our function
-     return 0;
+	// Attaching to the tracee process
+
+	if(ptrace(PTRACE_ATTACH, pid,NULL, NULL) < 0)
+	{}
+
+	if(waitpid(pid, NULL, 0) < 0)
+	{}
+
+	// Looking for foo in the tracee binary
+	if( execl("/bin/nm", "tracee",NULL) < 0)
+	{}
+
+	char addr[16];
+	char type[1];
+	char* name;
+	int part = 0;
+
+		
+	
+
+
+	// Finding the call to foo
+
+	// Writing our function
+
+	// Replacing foo by our function
+	return 0;
 }
