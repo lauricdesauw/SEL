48beec1b370164d87d186b5e7278458b3095efdb
adralak <yan.garito@ns-rennes.fr>
2019-10-09 10:16:03 +0200

    Update. Problem with scanning nm output.

--- tracer.c
+++ tracer.c
@@ -1,75 +1,98 @@
 #include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
+#include <string.h>
 #include <stdio.h>
+#include <errno.h>
 
-#define ERROR(msg) { fprintf(msg, stderr); goto Exit; }
+#define ERROR_ERRNO(msg) { fprintf(stderr, msg, strerror(errno)); goto Exit; }
+#define ERROR(msg) { fprintf(stderr, msg); goto Exit; }
 
 int main (int argc, char** argv)
 {
-	// Getting the pid of the tracee process
+     // Getting the pid of the tracee process
 	
-	printf("Finf the pid of the tracee programm\n");
+     printf("Looking for the pid of the tracee program\n");
+     
+     pid_t pid;
+     FILE* pgrep = popen("pgrep tracee", "r");
 
-	if( execl("/bin/pgrep", "tracee",NULL) < 0)
-	     ERROR("Unable to pgrep tracee\n")
+     if(fscanf(pgrep, "%d", &pid) == EOF)
+     {
+	  ERROR_ERRNO("Unable to get PID ! %s\n");
+     }
 
-	pid_t pid;
-	if(fread(&pid,sizeof(pid_t),1,stdout) < 0)
-	     ERROR("Unable to read PID in stdout\n")
+     pclose(pgrep);
+     
+     printf("Pid found, it's : %d\n", pid);
 
-	printf("Pid find it's : %d\n", pid);
+     // Attaching to the tracee process
 
-	// Attaching to the tracee process
-
-	printf("Ask for attaching\n");
+     printf("Ptracing...\n");
 	
-	if(ptrace(PTRACE_ATTACH, pid,NULL, NULL) < 0)
-	     ERROR("Could not trace PID\n")
+     if(ptrace(PTRACE_ATTACH, pid,NULL, NULL) < 0)
+     {
+	  ERROR_ERRNO("Could not trace PID ! %s\n");
+     }
 
-	if(waitpid(pid, NULL, 0) < 0)
-	     ERROR("Error while waiting for PID\n")
+     if(waitpid(pid, NULL, 0) < 0)
+     {
+	  ERROR("Error while waiting for PID\n");
+     }
 	
-	printf("Attached\n");
+     printf("Attached\n");
 
-	// Looking for foo in the tracee binary
+     // Looking for foo in the tracee binary
 	
-	printf("Looking for foo's addr\n");
-	if( execl("/bin/nm", "tracee",NULL) < 0)
-	     ERROR("Could not exec nm on tracee\n")
+     printf("Looking for foo's addr\n");
 
-	int addr;
-	char type;
-	char name[1000];
-	while(1)
-	{
-		fscanf(stdout, "%x %c %s", &addr, &type, name);
-		if(!addr)
-			continue;
+     FILE* nm = popen("nm tracee", "r");
+     int addr;
+     char type;
+     char name[1000];
+	
+     while(fscanf(nm, "%x %c %s", &addr, &type, name) != EOF)
+     {
+	  printf("%x %c %s %ld\n", addr, type, name, ftell(nm));
 
-		if(type == 'T')
-		{
-			if(name[0] == 'f')
+	  if(ftell(nm) < 0)
+	  {
+	       ERROR_ERRNO("Problem with file position ! %s\n");
+	  }
+	  
+	  if(!addr)
+	       continue;
 
-			{
-				if(name[1] == 'o')
+	  if(type == 'T')
+	  {
+	       if(!strcmp(name, "foo"))
+		    break;
+	  }
+     }
 
-				{
-					if(name[2] == 'o')
+     printf("foo addr is : %d\n", addr);
 
-					{
-						if(name[3] == '\0')
-						{
-							break;
-						}
-					}
-				}
-			}
-		}
-	}
+     char path[25] = {0};
+     sprintf(path, "/proc/%d/mem", pid); 
+	
+     FILE* mem = fopen(path, "a");
 
-	printf("foo addr is : %d\n", addr);
+     if(mem == NULL)
+     {
+	  ERROR("Could not open mem\n");
+     }
 
+     char c = fgetc(mem);
+	  
+     while(c != EOF)
+     {
+	  printf("%c", c);
+	  c = fgetc(mem);
+     }
+     
+     return 0;
 
 Exit:
-	return 0;
+     return 1;
 }
