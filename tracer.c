b5577a4b0f9c5475d5805ce0d7be3a694ec39fe9
Lauric Desauw <lauric@Jace.localdomain>
2019-10-09 11:17:34 +0200

    Just need to find how to read the mem

--- tracer.c
+++ tracer.c
@@ -11,88 +11,80 @@
 
 int main (int argc, char** argv)
 {
-     // Getting the pid of the tracee process
-	
-     printf("Looking for the pid of the tracee program\n");
-     
-     pid_t pid;
-     FILE* pgrep = popen("pgrep tracee", "r");
+	// Getting the pid of the tracee process
 
-     if(fscanf(pgrep, "%d", &pid) == EOF)
-     {
-	  ERROR_ERRNO("Unable to get PID ! %s\n");
-     }
+	printf("Looking for the pid of the tracee program\n");
 
-     pclose(pgrep);
-     
-     printf("Pid found, it's : %d\n", pid);
+	pid_t pid;
+	FILE* pgrep = popen("pgrep tracee", "r");
 
-     // Attaching to the tracee process
+	if(fscanf(pgrep, "%d", &pid) == EOF)
+	{
+		ERROR_ERRNO("Unable to get PID ! %s\n");
+	}
 
-     printf("Ptracing...\n");
-	
-     if(ptrace(PTRACE_ATTACH, pid,NULL, NULL) < 0)
-     {
-	  ERROR_ERRNO("Could not trace PID ! %s\n");
-     }
+	pclose(pgrep);
 
-     if(waitpid(pid, NULL, 0) < 0)
-     {
-	  ERROR("Error while waiting for PID\n");
-     }
-	
-     printf("Attached\n");
+	printf("Pid found, it's : %d\n", pid);
 
-     // Looking for foo in the tracee binary
-	
-     printf("Looking for foo's addr\n");
+	// Attaching to the tracee process
 
-     FILE* nm = popen("nm tracee", "r");
-     int addr;
-     char type;
-     char name[1000];
-	
-     while(fscanf(nm, "%x %c %s", &addr, &type, name) != EOF)
-     {
-	  printf("%x %c %s %ld\n", addr, type, name, ftell(nm));
+	printf("Ptracing...\n");
 
-	  if(ftell(nm) < 0)
-	  {
-	       ERROR_ERRNO("Problem with file position ! %s\n");
-	  }
-	  
-	  if(!addr)
-	       continue;
+	if(ptrace(PTRACE_ATTACH, pid,NULL, NULL) < 0)
+	{
+		ERROR_ERRNO("Could not trace PID ! %s\n");
+	}
 
-	  if(type == 'T')
-	  {
-	       if(!strcmp(name, "foo"))
-		    break;
-	  }
-     }
+	if(waitpid(pid, NULL, 0) < 0)
+	{
+		ERROR("Error while waiting for PID\n");
+	}
 
-     printf("foo addr is : %d\n", addr);
+	printf("Attached\n");
 
-     char path[25] = {0};
-     sprintf(path, "/proc/%d/mem", pid); 
-	
-     FILE* mem = fopen(path, "a");
+	// Looking for foo in the tracee binary
 
-     if(mem == NULL)
-     {
-	  ERROR("Could not open mem\n");
-     }
+	printf("Looking for foo's addr\n");
 
-     char c = fgetc(mem);
-	  
-     while(c != EOF)
-     {
-	  printf("%c", c);
-	  c = fgetc(mem);
-     }
-     
-     return 0;
+	FILE* nm = popen("nm tracee", "r");
+	int addr;
+	char type;
+	char name[1000];
+
+	while(1) 
+	{
+		while(fgetc(nm) != '0') ;
+		fscanf(nm,"%x %c %s",&addr,&type ,name);
+
+		if(!strcmp(name, "foo"))
+			break;
+	}
+	pclose(nm);
+	printf("foo addr is : %x\n", addr);
+
+	char path[25] = {0};
+	sprintf(path, "/proc/%d/mem", pid); 
+
+	FILE* mem = fopen(path, "r+");
+
+	if(mem == NULL)
+	{
+		ERROR_ERRNO("Could not open mem %s\n");
+	}
+
+	char c = fgetc(mem);
+	if(c==EOF)
+		ERROR_ERRNO("EOF %s\n");
+
+	while(c != EOF)
+	{
+		printf("%c", c);
+		c = fgetc(mem);
+	}
+
+	return 0;
 
 Exit:
-     return 1;
+	return 1;
 }
