d33ece3ecaf20f278aab4fc42712373959ead357
Desauw <lauric@Jace.localdomain>
2019-10-17 10:50:59 +0200

    Tracer.c

--- Chlg2/tracer.c
+++ Chlg2/tracer.c
@@ -11,106 +11,126 @@
 
 int main (int argc, char** argv)
 {
-	// Getting the pid of the tracee process
+     // Getting the pid of the tracee process
 
-	printf("Looking for the pid of the tracee program\n");
+     printf("Looking for the pid of the tracee program\n");
 
-	pid_t pid;
-	FILE* pgrep = popen("pgrep tracee", "r");
+     pid_t pid;
+     FILE* pgrep = popen("pgrep tracee", "r");
 
-	if(fscanf(pgrep, "%d", &pid) == EOF)
-	{
-		ERROR_ERRNO("Unable to get PID ! %s\n");
-	}
+     if(fscanf(pgrep, "%d", &pid) == EOF)
+     {
+	  ERROR_ERRNO("Unable to get PID ! %s\n");
+     }
 
-	pclose(pgrep);
+     pclose(pgrep);
 
-	printf("Pid found, it's : %d\n", pid);
+     printf("Pid found, it's : %d\n", pid);
 
-	// Attaching to the tracee process
+     // Attaching to the tracee process
 
-	printf("Ptracing...\n");
+     printf("Ptracing...\n");
 
-	if(ptrace(PTRACE_ATTACH, pid,NULL, NULL) < 0)
-	{
-		ERROR_ERRNO("Could not trace PID ! %s\n");
-	}
-
-
-	if(waitpid(pid, NULL, 0) < 0)
-	{
-		ERROR("Error while waiting for PID\n");
-	}
-
-	printf("Attached\n");
-
-	// Looking for foo in the tracee binary
-
-	printf("Looking for foo's addr\n");
-
-	FILE* nm = popen("nm tracee", "r");
-	int addr;
-	int addr_foo = 0;
-	int addr_goo = 0;
-	char type;
-	char name[1000];
-
-	while(1) 
-	{
-		while(fgetc(nm) != '0') ;
-		fscanf(nm,"%x %c %s",&addr,&type ,name);
-
-		if(!strcmp(name, "foo"))
-			addr_foo = addr;
-
-		if(!strcmp(name, "goo"))
-			addr_goo = addr;
-
-		if(addr_foo && addr_goo) 
-			break;
-	}
-	pclose(nm);
-	printf("foo addr is : %x\n", addr_foo);
-	printf("goo addr is : %x\n", addr_goo);
-
-	char path[25] = {0};
-	sprintf(path, "/proc/%d/mem", pid); 
-
-	FILE* mem = fopen(path, "r+");
-
-	if(mem == NULL)
-	{
-		ERROR_ERRNO("Could not open mem %s\n");
-	}
-	int trap = 0xcc;
-	fseek(mem,addr_goo,1);
-	fwrite(&trap, 1, 1, mem);
-	printf("goo is trapped\n");
-	fclose(mem);
-
-	ptrace(PTRACE_CONT, pid, NULL, NULL) ;
-
-	// getting the value in the register 
-
-	char register[1024];	
-	ptrace(PTRACE_GETREGS, pid, NULL,register);
+     if(ptrace(PTRACE_ATTACH, pid,NULL, NULL) < 0)
+     {
+	  ERROR_ERRNO("Could not trace PID ! %s\n");
+     }
 
 
-	// Change the goo code to call foo with parameter
+     if(waitpid(pid, NULL, 0) < 0)
+     {
+	  ERROR("Error while waiting for PID\n");
+     }
 
-	ptrace(PTRACE_CONT, pid, NULL, NULL) ;
+     printf("Attached\n");
+
+     // Looking for foo in the tracee binary
+
+     printf("Looking for foo's addr\n");
+
+     FILE* nm = popen("nm tracee", "r");
+     int addr;
+     int addr_foo = 0;
+     int addr_goo = 0;
+     char type;
+     char name[1000];
+
+     while(1) 
+     {
+	  while(fgetc(nm) != '0') ;
+	  fscanf(nm,"%x %c %s",&addr,&type ,name);
+
+	  if(!strcmp(name, "foo"))
+	       addr_foo = addr;
+
+	  if(!strcmp(name, "goo"))
+	       addr_goo = addr;
+
+	  if(addr_foo && addr_goo) 
+	       break;
+     }
+     pclose(nm);
+     printf("foo addr is : %x\n", addr_foo);
+     printf("goo addr is : %x\n", addr_goo);
+
+     char path[25] = {0};
+     sprintf(path, "/proc/%d/mem", pid); 
+
+     FILE* mem = fopen(path, "r+");
+
+     if(mem == NULL)
+     {
+	  ERROR_ERRNO("Could not open mem %s\n");
+     }
+     int trap = 0xcc;
+     unsigned long long int goo_code;
+     // Save goo code 
+     fseek(mem,addr_goo,1);
+     fread(&goo_code, 4, 1, mem); 
+     
+     // Trap goo 
+     fseek(mem,addr_goo,1);
+     fwrite(&trap, 1, 1, mem);
+     printf("goo is trapped\n");
+     fclose(mem);
+
+     ptrace(PTRACE_CONT, pid, NULL, NULL) ;
+
+     // getting the value in the register 
+
+     user_regs_struct regs;	
+     ptrace(PTRACE_GETREGS, pid, NULL,&regs);
 
 
-	// Restore the goo code and register value
+     // Change the goo code to call foo with parameter
+
+     ptrace(PTRACE_CONT, pid, NULL, NULL) ;
 
 
+     // Restore the goo code and register value
+     regs.rip = addr_goo;
+     ptrace(PTRACE_SETREGS, pid,NULL , &regs);
+     printf("Register are restored\n");
 
-	ptrace(PTRACE_CONT, pid, NULL, NULL) ;
-	ptrace(PTRACE_DETACH, pid,NULL, NULL) ;
 
-	return 0;
+     mem = fopen(path, "r+");
+
+     if(mem == NULL)
+     {
+	  ERROR_ERRNO("Could not open mem %s\n");
+     }
+     fseek(mem,addr_goo,1);
+     fwrite(&goo_code, 4, 1, mem);
+     printf("goo is restored\n");
+     fclose(mem);
+
+
+     ptrace(PTRACE_CONT, pid, NULL, NULL) ;
+     ptrace(PTRACE_DETACH, pid,NULL, NULL) ;
+
+     return 0;
 
 Exit:
-	return 1;
+     return 1;
 }
 
