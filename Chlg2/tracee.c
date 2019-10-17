de76db077d04737d072ad96f9ee28d7f145f48c3
adralak <yan.garito@ns-rennes.fr>
2019-10-17 11:46:50 +0200

    SHOULD BE GOOD

--- Chlg2/tracee.c
+++ Chlg2/tracee.c
@@ -4,11 +4,18 @@
 int foo(int i)
 
 {
-	printf("foo is doing some things : \n");	
-	sleep(5);
-	return i;
+     printf("foo is doing some things : %d\n", i);	
+     sleep(5);
+     return i;
 }
 
+int bar(int* i)
+{
+     char* c = (char *) i;
+     printf("bar is doing stuff : %d\n", *c);
+     sleep(5);
+     return 5;
+}
 
 void goo()
 { 
