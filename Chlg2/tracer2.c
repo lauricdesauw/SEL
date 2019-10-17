7e0af4aaf383162d46b9cf88328d7b32447c3416
adralak <yan.garito@ns-rennes.fr>
2019-10-17 11:47:01 +0200

    SHOULD BE GOOD

---
+++ Chlg2/tracer2.c
@@ -0,0 +1,174 @@
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/user.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+
+#define ERROR_ERRNO(msg) { fprintf(stderr, msg, strerror(errno)); goto Exit; }
+#define ERROR(msg) { fprintf(stderr, msg); goto Exit; }
+
+typedef struct user_regs_struct user_regs_struct;
+
+int main (int argc, char** argv)
+{
+     // Getting the pid of the tracee process
+
+     printf("Looking for the pid of the tracee program\n");
+
+     pid_t pid;
+     FILE* pgrep = popen("pgrep tracee", "r");
+
+     if(fscanf(pgrep, "%d", &pid) == EOF)
+     {
+	  ERROR_ERRNO("Unable to get PID ! %s\n");
+     }
+
+     pclose(pgrep);
+
+     printf("Pid found, it's : %d\n", pid);
+
+     // Attaching to the tracee process
+
+     printf("Ptracing...\n");
+
+     if(ptrace(PTRACE_ATTACH, pid,NULL, NULL) < 0)
+     {
+	  ERROR_ERRNO("Could not trace PID ! %s\n");
+     }
+
+
+     if(waitpid(pid, NULL, 0) < 0)
+     {
+	  ERROR("Error while waiting for PID\n");
+     }
+
+     printf("Attached\n");
+
+     // Looking for bar in the tracee binary
+
+     printf("Looking for bar's addr\n");
+
+     FILE* nm = popen("nm tracee", "r");
+     int addr;
+     int addr_bar = 0;
+     int addr_goo = 0;
+     char type;
+     char name[1000];
+
+     while(1) 
+     {
+	  while(fgetc(nm) != '0') ;
+	  fscanf(nm,"%x %c %s",&addr,&type ,name);
+
+	  if(!strcmp(name, "bar"))
+	       addr_bar = addr;
+
+	  if(!strcmp(name, "goo"))
+	       addr_goo = addr;
+
+	  if(addr_bar && addr_goo) 
+	       break;
+     }
+     pclose(nm);
+     printf("bar addr is : %x\n", addr_bar);
+     printf("goo addr is : %x\n", addr_goo);
+
+     char path[25] = {0};
+     sprintf(path, "/proc/%d/mem", pid); 
+
+     FILE* mem = fopen(path, "r+");
+
+     if(mem == NULL)
+     {
+	  ERROR_ERRNO("Could not open mem %s\n");
+     }
+     int trap = 0xcc;
+     unsigned long long int goo_code;
+     // Save goo code 
+     fseek(mem,addr_goo, SEEK_SET);
+     fread(&goo_code, 4, 1, mem); 
+     
+     // Trap goo 
+     fseek(mem,addr_goo, SEEK_SET);
+     fwrite(&trap, 1, 1, mem);
+     printf("goo is trapped\n");
+     fclose(mem);
+
+     ptrace(PTRACE_CONT, pid, NULL, NULL) ;
+
+     // getting the value in the register 
+
+     if(waitpid(pid, NULL, 0) < 0)
+     {
+	  ERROR("Error while waiting for PID\n");
+     }
+     
+     user_regs_struct regs;	
+
+     ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+
+     // Change the goo code to call bar with parameter
+     
+     unsigned long long rax = regs.rax, rdi = regs.rdi, rsp = regs.rsp;
+     regs.rax = addr_bar;
+     regs.rsp -= 1;
+     regs.rdi = regs.rsp + 1;
+     char call_trap[] = {0xff, 0xd0, 0xcc};
+
+     char value = 7;
+     mem = fopen(path, "w+");
+
+     if(mem == NULL)
+     {
+	  ERROR_ERRNO("Could not open mem %s\n");
+     }
+     fseek(mem, addr_goo + 1, SEEK_SET);
+     fwrite(call_trap, 1, 3, mem);
+
+     fseek(mem, regs.rdi, SEEK_SET);
+     fwrite(&value, 1, 1, mem);
+    
+     fclose(mem);
+     
+     ptrace(PTRACE_SETREGS, pid, NULL, &regs);
+     ptrace(PTRACE_CONT, pid, NULL, NULL) ;
+
+     if(waitpid(pid, NULL, 0) < 0)
+     {
+	  ERROR("Error while waiting for PID\n");
+     }
+     
+     // Restore the goo code and register value
+     regs.rax = rax;
+     regs.rdi = rdi;
+     regs.rsp = rsp;
+     regs.rip = addr_goo;
+     ptrace(PTRACE_SETREGS, pid,NULL , &regs);
+     printf("Register are restored\n");
+
+     mem = fopen(path, "r+");
+     if(mem == NULL)
+     {
+	  ERROR_ERRNO("Could not open mem %s\n");
+     }
+     fseek(mem,addr_goo,1);
+     fwrite(&goo_code, 4, 1, mem);
+     printf("goo is restored\n");
+     fclose(mem);
+
+
+     ptrace(PTRACE_CONT, pid, NULL, NULL) ;
+     ptrace(PTRACE_DETACH, pid,NULL, NULL) ;
+
+
+     return 0;
+
+Exit:
+     return 1;
+}
+
+
+
